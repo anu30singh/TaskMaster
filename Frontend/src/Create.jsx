b0e64b42ff72f41433083b716eb1188b82749b3a
Anushka <anushka30singh07@gmail.com>
2024-04-19 18:06:31 +0530

    add

--- Frontend/src/Create.jsx
+++ Frontend/src/Create.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import './App.css'
+import React, { useState } from "react";
+import axios from 'axios'
 function Create() {
+    const [task,setTask] = useState()
+    const handleAdd= ()=> {
+        axios.post('http://localhost:3001/add' , {task: task})
+        .then(result=> console.log(result))
+        .catch(err=> console.log(err))
+
+
+    }
     return (
-        <div>
-           <input type="text" name ="" id = ""/>
-           <button type="button"> Add </button>
-        </div>
+        <div className="create_form">
+           <input type="text" placeholder = "Enter here" onChange={(e)=> setTask(e.target.value)}/>
+           <button type="button" onClick={handleAdd}> Add </button>
+        </div> //from the function we will call the database
     )
 }
 
--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -5,15 +5,23 @@ import './App.css'
 function Home() {
     const [todos, setTodos] = useState([])
     return (
-        <div>
+        <div className="home">
             <h2>Keep Track, Stay Inspired</h2>
             <Create />
             {
                 todos.length === 0 ?  //ternary if length 0 that means no record to dispaly
                 <div> <h2> No Record</h2> </div> :
                 todos.map(todo=>(
-                   <div>
-                    {todo}
+                   <div className="task">
+                    <div className="checkbox" onClick={handleEdit}>
+                        <bsFillBox className= 'icon'/>
+                    <p>{todo.task}</p>
+
+                    </div>
+                    
+                    <div> 
+                        <span> <BsFillTrash className= 'icon'/></span>
+                    </div>
                    </div>
                 ))
             }
