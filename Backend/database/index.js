e3ea44fef6a8f387cd09015aa52d8255e8ec4883
Anushka <anushka30singh07@gmail.com>
2024-07-04 22:40:17 +0530

    backend

--- Backend/database/index.js
+++ Backend/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const {MONGO_URI} = require('../config')
-mongoose.connect(MONGO_URI);
+const {MONGO_URI} = require('../config');
+const { configDotenv } = require('dotenv');
+mongoose.connect(MONGO_URI).then(()=>console.log("DB connected"))
+.catch((err)=> console.log("db not connected"))
 
 
 const ToDoSchema = new mongoose.Schema({
